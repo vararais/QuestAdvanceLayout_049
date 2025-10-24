7e18a5b2a99c177c001d809bf47f3209915fadb7
vararais <varadigmaisnann@gmail.com>
2025-10-24 15:35:40 +0700

    Menambahkan Spacer

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -65,6 +66,8 @@ fun AktivitasPertama(modifier: Modifier){
                         .size(size = 100.dp)
                         .padding(all = 5.dp)
                 )
+
+                Spacer(modifier = Modifier.width(width = 30.dp))
             }
         }
     }
