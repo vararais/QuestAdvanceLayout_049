d3fd8b4c4aff44199837101d6573a0af4fe6d570
vararais <varadigmaisnann@gmail.com>
2025-10-24 15:55:27 +0700

    Mengatur agar Padding bawah serta Meratakan di tengah bawah

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
@@ -91,6 +91,13 @@ fun AktivitasPertama(modifier: Modifier){
         Box(
             modifier= Modifier
                 .fillMaxSize()
-        )
+        ){
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
     }
 }
