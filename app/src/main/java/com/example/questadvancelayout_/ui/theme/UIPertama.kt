94ef078720185c044c5d957e29553c4821c15a61
vararais <varadigmaisnann@gmail.com>
2025-10-24 15:53:13 +0700

    Membuat Box

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -87,5 +88,9 @@ fun AktivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Box(
+            modifier= Modifier
+                .fillMaxSize()
+        )
     }
 }
