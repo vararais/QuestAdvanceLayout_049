7c536f82a89d4b955fb41744931a11470fda22da
vararais <varadigmaisnann@gmail.com>
2025-10-24 15:26:45 +0700

    Membuat Card

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/UIPertama.kt
@@ -3,13 +3,16 @@ package com.example.questadvancelayout_.ui.theme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -37,6 +40,14 @@ fun AktivitasPertama(modifier: Modifier){
 
         Spacer(modifier = Modifier.height(height = 25.dp))
 
-        Card {  }
+        Card(
+            modifier = Modifier
+                .height(height = 160.dp)
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ){}
     }
 }
