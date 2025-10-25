8fa646c7687cf087626d02f488d152a7e9a315d2
vararais <varadigmaisnann@gmail.com>
2025-10-25 14:57:23 +0700

    Membuat Fungsi "ProfileScreen"

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
@@ -1,2 +1,24 @@
 package com.example.questadvancelayout_.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212))
+            .padding(top = 60.dp)
+            .padding(horizontal = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+    }
+}
