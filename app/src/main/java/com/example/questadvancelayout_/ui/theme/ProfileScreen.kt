2022df04b63efd3f9e848adf35eeb6c35f6c2664
vararais <varadigmaisnann@gmail.com>
2025-10-25 20:37:28 +0700

    Mengubah variabel teks menjadi menggunakan string resource

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -68,7 +69,7 @@ fun ProfileScreen(modifier: Modifier) {
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "Abra Yudhistira",
+            text = stringResource(id = R.string.namautama),
             fontSize = 26.sp,
             fontWeight = FontWeight.Bold,
             color = Color.White
