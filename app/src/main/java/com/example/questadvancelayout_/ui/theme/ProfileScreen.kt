5082b3f622c6e8d3d5b95e4f2a469cc4ecbf37d9
vararais <varadigmaisnann@gmail.com>
2025-10-25 15:14:41 +0700

    Membuat Spacer

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
@@ -7,12 +7,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -81,7 +86,11 @@ fun ProfileScreen(modifier: Modifier = Modifier) {
             color = Color.White
         )
 
-        Spacer(modifier = Modifier.height(32.dp
+        Spacer(modifier = Modifier.height(32.dp))
+
+    }
+
+
 
 
 }
