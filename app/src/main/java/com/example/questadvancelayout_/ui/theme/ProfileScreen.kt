cb8073eb1f564ca913400f063bb01b60396bf329
vararais <varadigmaisnann@gmail.com>
2025-10-25 15:01:08 +0700

    Membuat variabel read only dengan gambar

--- app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
+++ app/src/main/java/com/example/questadvancelayout_/ui/theme/ProfileScreen.kt
@@ -1,14 +1,23 @@
 package com.example.questadvancelayout_.ui.theme
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.questadvancelayout_.R
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier) {
@@ -20,5 +29,16 @@ fun ProfileScreen(modifier: Modifier = Modifier) {
             .padding(horizontal = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val gambar = painterResource(id = R.drawable.profile_pic)
+        Image(
+            painter = gambar,
+            contentDescription = "Foto Profil",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
